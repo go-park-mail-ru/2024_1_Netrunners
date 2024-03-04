bf457d35ad4bd1ab71389e4333187a775851df7c
Dima <farset007@mail.ru>
2024-03-04 10:40:19 +0300

    №13 feat: add film storage

---
+++ internal/repository/mockDB/films.go
@@ -0,0 +1,64 @@
+package mockdb
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/go-park-mail-ru/2024_1_Netrunners/internal/domain"
+)
+
+type FilmsMockDB struct {
+	storage map[string]domain.FilmPreview
+	mutex   *sync.RWMutex
+}
+
+func InitFilmsMockDB() *FilmsMockDB {
+	return &FilmsMockDB{
+		storage: make(map[string]domain.FilmPreview),
+		mutex:   &sync.RWMutex{},
+	}
+}
+
+func (db *FilmsMockDB) AddFilm(film domain.FilmPreview) error {
+	db.mutex.RLock()
+	_, ok := db.storage[film.Id]
+	db.mutex.RUnlock()
+
+	if ok {
+		return errors.New("film with given Id already exists")
+	}
+
+	db.mutex.Lock()
+	db.storage[film.Id] = film
+	db.mutex.Unlock()
+
+	return nil
+}
+
+func (db *FilmsMockDB) RemoveFilm(id string) error {
+	db.mutex.RLock()
+	_, ok := db.storage[id]
+	db.mutex.RUnlock()
+
+	if !ok {
+		return errors.New("film with given Id doesn't exists")
+	}
+
+	db.mutex.Lock()
+	delete(db.storage, id)
+	db.mutex.Unlock()
+
+	return nil
+}
+
+func (db *FilmsMockDB) GetFilmPreview(id string) (domain.FilmPreview, error) {
+	db.mutex.RLock()
+	film, ok := db.storage[id]
+	db.mutex.RUnlock()
+
+	if !ok {
+		return domain.FilmPreview{}, errors.New("film with given Id doesn't exists")
+	}
+
+	return film, nil
+}
