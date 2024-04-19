5642c1b6caf04b7c4e7fda063dc84e011343c130
dozerdozerdozer <cakethefake@gmail.com>
2024-04-19 14:31:22 +0300

    imports fixes

--- internal/repository/postgres/actors.go
+++ internal/repository/postgres/actors.go
@@ -3,10 +3,11 @@ package database
 import (
 	"context"
 
-	"github.com/go-park-mail-ru/2024_1_Netrunners/internal/domain"
-	myerrors "github.com/go-park-mail-ru/2024_1_Netrunners/internal/errors"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+
+	"github.com/go-park-mail-ru/2024_1_Netrunners/internal/domain"
+	myerrors "github.com/go-park-mail-ru/2024_1_Netrunners/internal/errors"
 )
 
 type PgxIface interface {
