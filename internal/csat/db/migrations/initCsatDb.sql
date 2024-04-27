4b25cdf4f3234bc5556b0a021e0aa1deda2d2fe8
dozerdozerdozer <volozhaninov.daniil@gmail.com>
2024-04-27 15:30:01 +0300

    rebasing

---
+++ internal/csat/db/migrations/initCsatDb.sql
@@ -0,0 +1,104 @@
+CREATE TABLE IF NOT EXISTS profile_question
+(
+	id          INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	external_id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
+	question    TEXT                                  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS additional_question
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	external_id          UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
+	question_external_id UUID                                  NOT NULL,
+	question             TEXT                                  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS additional_answer
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	external_id          UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
+	id_inside_question   INTEGER                               NOT NULL,
+	question_external_id UUID                                  NOT NULL,
+	variant              TEXT                                  NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS profile_stat
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	question_external_id UUID    NOT NULL,
+	score                INTEGER NOT NULL,
+	is_additional_score  BOOL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS film_data_question
+(
+	id          INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	external_id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
+	question    TEXT                                  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS film_data_additional_question
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_inside_question   INTEGER NOT NULL,
+	QUESTION_EXTERNAL_ID UUID    NOT NULL,
+	question             TEXT    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS film_data_stat
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	question_external_id UUID    NOT NULL,
+	score                INTEGER NOT NULL,
+	is_additional_score  BOOL DEFAULT FALSE
+
+);
+
+CREATE TABLE IF NOT EXISTS actor_question
+(
+	id          INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	external_id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
+	question    TEXT                                  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS actor_additional_question
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_inside_question   INTEGER NOT NULL,
+	QUESTION_EXTERNAL_ID UUID    NOT NULL,
+	question             TEXT    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS actor_stat
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	question_external_id UUID    NOT NULL,
+	score                INTEGER NOT NULL,
+	is_additional_score  BOOL DEFAULT FALSE
+
+);
+
+CREATE TABLE IF NOT EXISTS film_question
+(
+	id          INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	external_id UUID UNIQUE DEFAULT gen_random_uuid() NOT NULL,
+	question    TEXT                                  NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS film_additional_question
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id_inside_question   INTEGER NOT NULL,
+	QUESTION_EXTERNAL_ID UUID    NOT NULL,
+	question             TEXT    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS film_stat
+(
+	id                   INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	question_external_id UUID    NOT NULL,
+	score                INTEGER NOT NULL,
+	is_additional_score  BOOL DEFAULT FALSE
+
+);
--- internal/csat/repository/csat.go
+++ internal/csat/repository/csat.go
@@ -3,7 +3,10 @@ package repository
 import (
 	"context"
 
+	"fmt"
 	"github.com/go-park-mail-ru/2024_1_Netrunners/internal/domain"
+	myerrors "github.com/go-park-mail-ru/2024_1_Netrunners/internal/errors"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 )
@@ -27,30 +30,61 @@ func NewStatStorage(pool PgxIface) (*StatStorage, error) {
 	}, nil
 }
 
+const getAllProfileQuestions = `
+		SELECT uuid, question FROM profile_question`
+
+const getAdditionalQuestionByUuid = `
+		SELECT external_id, question FROM additional_question WHERE question_external_id = $1;`
+
 func (storage *StatStorage) AddQuestion(question domain.AddQuestion) error {
-	return nil
-}
-func (storage *StatStorage) GetPageQuestions(page string) ([]domain.Question, error) {
-	return []domain.Question{
-		{
-			Title: "sad",
-			AdditionalQuestion: domain.AdditionalQuestion{
-				Title: "dsa",
-				CheckVars: []domain.Variant{
-					{
-						Id:    1,
-						Title: "sfddadf",
-					},
-				},
-			},
-		},
-	}, nil
+
 }
 
-func (storage *StatStorage) AddStatistics(page string, statistics []domain.AddQuestionStatistics) error {
-	return nil
+func (storage *StatStorage) GetPageQuestions(page string) ([]domain.Question, error) {
+	switch {
+	case page == "profileData":
+		var allQuestions []domain.Question
+		rows, err := storage.pool.Query(context.Background(), getAllProfileQuestions)
+		if err != nil {
+			return nil, fmt.Errorf("failed to begin transaction to add film: %w: %w", err,
+				myerrors.ErrNoSuchItemInTheCache)
+		}
+		for rows.Next() {
+			var (
+				que   domain.Question
+				uuid  string
+				title string
+			)
+			err = rows.Scan(&uuid, &title)
+			que.Uuid = uuid
+			que.Title = title
+
+			var additionalQuestions domain.AdditionalQuestion
+			err = storage.pool.QueryRow(context.Background(), getAdditionalQuestionByUuid, que.Uuid)
+			for rows.Next() {
+				var (
+					addQue     domain.AdditionalQuestion
+					addQueUuid string
+					addQueText string
+				)
+				err = rows.Scan(&addQueUuid, &addQueText)
+				addQue.Uuid = addQueUuid
+				addQue.Title = addQueText
+				var variants []string
+			}
+			allQuestions = append(allQuestions, que)
+		}
+	case page == "actorData":
+
+	case page == "filmsAll":
+	case page == "filmData":
+	}
+}
+
+func (storage *StatStorage) AddStatistics(statistics []domain.AddQuestionStatistics) error {
+
 }
 
 func (storage *StatStorage) GetStatisticsByPage(page string) ([]domain.QuestionStatistics, error) {
-	return []domain.QuestionStatistics{}, nil
+
 }
