c1484823cc352616dba2625641bc2f57c138ef13
Dima <farset007@mail.ru>
2024-05-08 09:43:38 +0300

    feat: add cadvisor

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -46,7 +46,7 @@ func main() {
 
 	pool, err := pgxpool.New(context.Background(), fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		"postgres",
+		"localhost",
 		"5432",
 		"postgres",
 		"root1234",
