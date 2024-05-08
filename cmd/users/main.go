c88fc26d467fbdc784f727f565924c9259241d9f
Dima <farset007@mail.ru>
2024-05-08 12:08:47 +0300

    a

--- cmd/users/main.go
+++ cmd/users/main.go
@@ -50,8 +50,8 @@ func main() {
 
 	pool, err := pgxpool.New(context.Background(), fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		"localhost",
-		"5432",
+		"postgres",
+		"5435",
 		"postgres",
 		"root1234",
 		"netrunnerflix",
